0e53c42893b7bd3dbbe5778746eb147052b03ea0
Danielle Tait <Danielletait@hotmail.co.uk>
2020-05-17 22:48:20 +0000

    added messages append

--- run.py
+++ run.py
@@ -2,14 +2,27 @@ import os
 from flask import Flask
 
 app = Flask(__name__)
+messages = []
+
+
+def add_messages(username, message):
+    messages.append("{}: {}".format(username, message))
 
 
 @app.route("/")
 def index():
-    return "<h1>Hello There</h1>"
+    """ Main page with instructions """
+    return "To send a message use: /USERNAME/MESSAGE"
 
 
-if __name__ =="__main__":
-    app.run(host=os.getenv("IP"),
-       port=int(os.getenv("PORT")),
-       debug=True)
+@app.route("/<username>")
+def user(username):
+    return "Welcome {0}".format(username)
+
+
+@app.route("/<username>/<message>")
+def send_message(username, message):
+    return "{0}: {1}".format(username, message)
+
+
+app.run(host=os.getenv("IP"), port=int(os.getenv("PORT")), debug=True)
